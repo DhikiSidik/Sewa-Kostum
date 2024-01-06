71a585c690c6c1fb468df070f9bfc040575bc0e4
DhikiSidik <120517628+DhikiSidik@users.noreply.github.com>
2024-01-06 20:55:51 +0700

    JWT

--- BackEnd/routes/kostum.js
+++ BackEnd/routes/kostum.js
@@ -1,192 +1,127 @@
 const express = require('express');
 const router = express.Router();
-
-//import express validator
 const { body, validationResult } = require('express-validator');
-
-//import database
 const connection = require('../config/database');
+const { isLoggedIn } = require('../middleware/users');
 
-/**
- * INDEX barang
- */
 router.get('/', function (req, res) {
-    //query
-    connection.query('SELECT * FROM barang', function (err, rows) {
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message: 'List Data barang',
-                data: rows
-            })
-        }
+  connection.query('SELECT * FROM barang', function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: 'List Data barang',
+        data: rows,
+      });
+    }
+  });
+});
+
+router.post('/add', isLoggedIn, [
+  body('nama').notEmpty(),
+  body('gambar').notEmpty(),
+  body('deskripsi').notEmpty(),
+  body('harga').notEmpty(),
+  body('status').notEmpty(),
+], async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+
+    const formData = {
+      nama: req.body.nama,
+      gambar: req.body.gambar,
+      deskripsi: req.body.deskripsi,
+      harga: req.body.harga,
+      status: req.body.status,
+    };
+
+    const result = await connection.query('INSERT INTO barang SET ?', formData);
+
+    return res.status(201).json({
+      status: true,
+      message: 'Insert Data Successfully',
+      data: {
+        id: result.insertId,
+        ...formData,
+      },
     });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({
+      status: false,
+      message: 'Internal Server Error',
+    });
+  }
 });
 
-/**
- * STORE barang
- */
-router.post('/add', [
-
-    //validation
-    // body('id').notEmpty(),
-    body('kostum').notEmpty(),
-    body('gambar').notEmpty(),
-    body('deskripsi').notEmpty(),
-    body('harga').notEmpty(),
-    body('status').notEmpty()
-
-], (req, res) => {
-
+router.patch('/edit/:id', isLoggedIn, [
+  body('nama').notEmpty(),
+  body('gambar').notEmpty(),
+  body('deskripsi').notEmpty(),
+  body('harga').notEmpty(),
+  body('status').notEmpty(),
+], async (req, res) => {
+  try {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            errors: errors.array()
-        });
+      return res.status(422).json({
+        errors: errors.array(),
+      });
     }
 
-    //define formData
-    let formData = {
-        // id: req.body.id,
-        kostum: req.body.kostum,
-        gambar: req.body.gambar,
-        deskripsi: req.body.deskripsi,
-        harga: req.body.harga,
-        status: req.body.status
-    }
+    const id = req.params.id;
+    const formData = {
+      nama: req.body.nama,
+      gambar: req.body.gambar,
+      deskripsi: req.body.deskripsi,
+      harga: req.body.harga,
+      status: req.body.status,
+    };
 
-    // insert query
-    connection.query('INSERT INTO barang SET ?', formData, function (err, rows) {
-        //if(err) throw err
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        } else {
-            return res.status(201).json({
-                status: true,
-                message: 'Insert Data Successfully',
-                data: rows[0]
-            })
-        }
-    })
+    await connection.query(`UPDATE barang SET ? WHERE id = ${id}`, formData);
 
+    return res.status(200).json({
+      status: true,
+      message: 'Update Data Successfully!',
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({
+      status: false,
+      message: 'Internal Server Error',
+    });
+  }
 });
 
-/**
- * SHOW barang
- */
-router.get('/(:id)', function (req, res) {
+router.delete('/delete/:id', isLoggedIn, function (req, res) {
+  const id = req.params.id;
 
-    let id = req.params.id;
-
-    connection.query(`SELECT * FROM barang WHERE id = ${id}`, function (err, rows) {
-
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        }
-
-        // if barang not found
-        if (rows.length <= 0) {
-            return res.status(404).json({
-                status: false,
-                message: 'Data barang Not Found!',
-            })
-        }
-        // if barang found
-        else {
-            return res.status(200).json({
-                status: true,
-                message: 'Detail Data barang',
-                data: rows[0]
-            })
-        }
-    })
-})
-
-/**
- * UPDATE barang
- */
-router.patch('/edit/:id', [
-
-    //validation
-    body('kostum').notEmpty(),
-    body('gambar').notEmpty(),
-    body('deskripsi').notEmpty(),
-    body('harga').notEmpty(),
-    body('status').notEmpty()
-
-], (req, res) => {
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(422).json({
-            errors: errors.array()
-        });
+  connection.query(`DELETE FROM barang WHERE id = ${id}`, function (err, rows) {
+    if (err) {
+      console.error('Error deleting item:', err);
+      return res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+      });
+    } else {
+      console.log('Item deleted successfully');
+      return res.status(200).json({
+        status: true,
+        message: 'Delete Data Successfully!',
+      });
     }
-
-    //id barang
-    let id = req.params.id;
-
-    //data barang
-    let formData = {
-        kostum: req.body.kostum,
-        gambar: req.body.gambar,
-        deskripsi: req.body.deskripsi,
-        harga: req.body.harga,
-        status: req.body.status
-    }
-
-    // upharga query
-    connection.query(`UPDATE barang SET ? WHERE id = ${id}`, formData, function (err, rows) {
-        //if(err) throw err
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message: 'Upharga Data Successfully!'
-            })
-        }
-    })
-
-});
-
-/**
- * DELETE barang
- */
-router.delete('/delete/(:id)', function (req, res) {
-
-    let id = req.params.id;
-
-    connection.query(`DELETE FROM barang WHERE id = ${id}`, function (err, rows) {
-        //if(err) throw err
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message: 'Delete Data Successfully!',
-            })
-        }
-    })
+  });
 });
 
 module.exports = router;
